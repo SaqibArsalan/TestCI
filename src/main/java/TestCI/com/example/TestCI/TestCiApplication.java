8ff00e146a96ade230145b1d92cf4c8350077f7a
SaqibArsalan <saqibarsalan1997@gmail.com>
2021-04-21 12:07:36 +0500

    Add a print statement

--- src/main/java/TestCI/com/example/TestCI/TestCiApplication.java
+++ src/main/java/TestCI/com/example/TestCI/TestCiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestCiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!!");
 		SpringApplication.run(TestCiApplication.class, args);
 	}
 
